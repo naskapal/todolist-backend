65d7cfdcda8e08b28c10e4e90df1f05fe5ed4493
Rizky <co.terbang@gmail.com>
2017-11-26 17:41:13 +0700

    trying out es2015 import, will replace all later

---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require('express'),
+      path = require('path'),
+      favicon = require('serve-favicon'),
+      logger = require('morgan'),
+      cookieParser = require('cookie-parser'),
+      bodyParser = require('body-parser'),
+      cors = require('cors'),
+      index = require('./routes/index'),
+      users = require('./routes/users')
+
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(cors())
+
+app.use('/', index);
+app.use('/users', users);
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.send({
+    error: err
+  });
+});
+
+module.exports = app;
---
+++ routes/users.js
@@ -0,0 +1,7 @@
+const router = require('express').Router()
+import jwt from 'jsonwebtoken'
+import User from '../controllers/userController'
+
+router.get('/login', )
+
+module.exports = router;
